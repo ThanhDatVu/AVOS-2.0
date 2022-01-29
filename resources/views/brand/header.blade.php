5eabe1d7403dbdb3f2704f8e488107a435ebcae4
TienTienBear <dat2611gd@gmail.com>
2022-01-29 23:46:34 +0700

    sửa logo phần admin

---
+++ resources/views/brand/header.blade.php
@@ -0,0 +1,18 @@
+@push('head')
+    <link
+        href="/favicon.ico"
+        id="favicon"
+        rel="icon"
+    >
+@endpush
+
+<p class="h2 n-m font-thin v-center">
+    <a class="navbar-brand" href="/admin">
+
+        <span class="m-l d-none d-sm-block">
+        AVOS
+        <small class="v-top opacity">Quản lý</small>
+    </span>
+    </a>
+
+</p>
