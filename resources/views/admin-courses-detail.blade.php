5d6171ede6e861560cd330cc5d9e5cb8dc535806
TienTienBear <dat2611gd@gmail.com>
2022-02-14 18:46:08 +0700

    sửa nav, thêm nút thêm lesson

--- resources/views/admin-courses-detail.blade.php
+++ resources/views/admin-courses-detail.blade.php
@@ -5,10 +5,14 @@
             {{$course->title}}
             @if(isset(Auth::user()->teacher->id))
                 @if ((Auth::user()->teacher->id != $course->teacher_id))
-                    <a href="{{route('enroll',['id'=>$course->id])}}" class="float-right shadow bg-green-00 hover:bg-green-600 hover:text-white btn text-uppercase enroll">Enroll the course</a>
+                    <a href="{{route('enroll',['id'=>$course->id])}}"
+                       class="float-right shadow bg-green-00 hover:bg-green-600 hover:text-white btn text-uppercase enroll">Enroll
+                        the course</a>
                 @endif
             @else
-                <a href="{{route('enroll',['id'=>$course->id])}}" class="float-right shadow bg-green-00 hover:bg-green-600 hover:text-white btn text-uppercase enroll">Enroll the course</a>
+                <a href="{{route('enroll',['id'=>$course->id])}}"
+                   class="float-right shadow bg-green-00 hover:bg-green-600 hover:text-white btn text-uppercase enroll">Enroll
+                    the course</a>
             @endif
         </h2>
     </x-slot>
@@ -24,7 +28,8 @@
                                         bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2">
                             <!-- media -->
                             <div class="h-64 w-auto md:w-1/2">
-                                <img class="inset-0 h-full w-full object-cover object-center" src="{{Storage::url($course->image)}}" />
+                                <img class="inset-0 h-full w-full object-cover object-center"
+                                     src="{{Storage::url($course->image)}}"/>
                             </div>
                             <!-- content -->
                             <div class="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
@@ -39,49 +44,68 @@
                             </div>
                         </div><!--/ card-->
 
-                    <!--================ Start Course Details Area =================-->
-                    <section class="course-details-area">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-lg-12 course-details-left">
-                                    <div class="content-wrapper">
+                        <!--================ Start Course Details Area =================-->
+                        <section class="course-details-area">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-12 course-details-left">
+                                        <div class="content-wrapper">
 
 
-                                        <h4 class="title">Tổng quan khoá học</h4>
-                                        <div class="content">
-                                            <ul class="course-list">
-                                                @forelse ($course->lessons as $lesson)
+                                            <h4 class="title">Tổng quan khoá học</h4>
+                                            <div class="content">
+                                                <ul class="course-list">
+                                                    @forelse ($course->lessons as $lesson)
 
-                                                <li class="justify-content-between d-flex">
-                                                    <p>{{$lesson->title}}</p>
-                                                        <div>
-                                                            <span class="text-uppercase justify-content-between d-flex" >
-                                                                <a href="{{route('lesson',["id"=>$lesson])}}" class="btn ti-eye">&nbsp;Read</a>
+                                                        <li class="justify-content-between d-flex">
+                                                            <p>{{$lesson->title}}</p>
+                                                            <div>
+                                                            <span class="text-uppercase justify-content-between d-flex">
+                                                                <a href="{{route('lesson',["id"=>$lesson])}}"
+                                                                   class="btn ti-eye">&nbsp;Read</a>
                                                             @if(isset(Auth::user()->teacher->id))
-                                                                @if (Auth::user()->teacher->id == $course->teacher_id)
-                                                                <a href="{{route("edit-lesson",["id"=>$lesson])}}" class="btn ti-pencil">&nbsp;Update</a><a href="{{route("edit-lesson",$lesson->id)}}" class="ml-3 btn hover:bg-red-600 ti-trash">&nbsp;Delete</a>
+                                                                    @if (Auth::user()->teacher->id == $course->teacher_id)
+                                                                        <a href="{{route("edit-lesson",["id"=>$lesson])}}"
+                                                                           class="btn ti-pencil">&nbsp;Update</a><a
+                                                                            href="{{route("edit-lesson",$lesson->id)}}"
+                                                                            class="ml-3 btn hover:bg-red-600 ti-trash">&nbsp;Delete</a>
+                                                                    @endif
                                                                 @endif
-                                                            @endif
                                                             </span>
+                                                            </div>
+                                                        </li>
+                                                    @empty
+                                                        <div class="w-full p-3 mb-3 text-white bg-red-600 rounded">Hiên
+                                                            chưa có bài học nào.
                                                         </div>
-                                                </li>
-                                                @empty
-                                                    <div class="w-full p-3 mb-3 text-white bg-red-600 rounded">Hiên chưa có bài học nào.</div>
-                                                @endforelse
-                                            </ul>
+                                                    @endforelse
+                                                </ul>
+
+                                            </div>
+
                                         </div>
+
                                     </div>
+                                    <div class='grid place-items-center w-100 m-3'>
+                                    <span class="text-uppercase justify-content-between d-flex">
+                                        <a href="{{route('make-new-lesson',["id"=>$lesson])}}"
+                                           class="btn ti-plus bg-gray-100">&nbsp;Thêm bài học mới</a>
+
+                                    </span>
+
+                                    </div>
+
+
                                 </div>
 
                             </div>
-                        </div>
-                    </section>
-                    <!--================ End Course Details Area =================-->
+                        </section>
+                        <!--================ End Course Details Area =================-->
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     @include("eclipse-interface.layouts.footer")
 </x-app-layout>
 {{-- @include("components.footer") --}}
